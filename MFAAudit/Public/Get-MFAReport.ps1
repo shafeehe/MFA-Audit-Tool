2c9c8eb1966bd46ce9b92f87c5ee028b27534b57
Mohammed Shafeehe <shafeehpms@gmail.com>
2026-01-06 13:36:56 +0530

    Added a Guard Rail safety-check feature for MFAAudit Tool

--- MFAAudit/Public/Get-MFAReport.ps1
+++ MFAAudit/Public/Get-MFAReport.ps1
@@ -12,6 +12,31 @@ Get-MFAReport
 Author: Mohammed Shafeehe
 Module: AWS IAM MFA Audit Tool
 #>
+#Guardrail for Multi-account Access: confirm before running the audit
+try {
+
+    $identity = Get-STSCallerIdentity
+    $AccountID = $identity.Account
+
+    $alias = (Get-IAMAccountAlias -ErrorAction SilentlyContinue)
+    if ($alias) { $AccountName = $alias }
+    else { $AccountName = "No Alias ($AccountID)" }
+
+    Write-Host "`nYou are about run MFA Audit on:" -ForegroundColor Yellow
+    Write-Host "the AWS Account: $AccountName"
+    Write-Host "Account ID: $AccountID`n"
+
+    $confirm = Read-Host "type YES to continue"
+    if ($confirm -ne "YES") {
+        Write-Host "The operation is cancelled." -ForegroundColor Red 
+        return }
+    }
+    
+catch {
+        Write-Host "Unable to verify AWS Account. Check credentials or AWS Configure. " -ForegroundColor Red
+        return
+      }
+
 function Get-MFAReport {
     $iamUsers = Get-IAMUserList
     foreach ($user in $iamUsers) {
